3b76714cd26226b6f48e629005d29fb246eb0280
Simon Gerhard <simon.gerhard@outlook.com>
2022-11-02 20:17:31 +0100

    Unittests

--- Aufgaben/01-unittest/Calculator.Tests/CalculatorTest.cs
+++ Aufgaben/01-unittest/Calculator.Tests/CalculatorTest.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 
+
 namespace Calculator.Tests
 {
     public class CalculatorTest
@@ -13,14 +14,11 @@ namespace Calculator.Tests
             int b = 3;
             int expected = 5;
             Calculator calculator = new Calculator();
-
             // Act
             int actual = calculator.Add(a, b);
-
             // Assert
             Assert.Equal(expected, actual);
         }
-
         [Fact]
         public void TestSubtractFourFromSixIsTwo()
         {
@@ -29,12 +27,51 @@ namespace Calculator.Tests
             int b = 4;
             int expected = 2;
             Calculator calculator = new Calculator();
-
             // When
             int actual = calculator.Subtract(a, b);
-
             // Then
             Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestMultiplikationFourTimesTwoIsEight()
+        {
+            // Given
+            int a = 2;
+            int b = 4;
+            int expected = 8;
+            Calculator calculator = new Calculator();
+            // When
+            int actual = calculator.Multiply(a, b);
+            // Then
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestDevideFourTimesTwoIsTwo()
+        {
+            // Given
+            int a = 4;
+            int b = 2;
+            int expected = 2;
+            Calculator calculator = new Calculator();
+            // When
+            int actual = calculator.Devide(a, b);
+            // Then
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestDevideFourTimesZeroIsExeption()
+        {
+            // Given
+            int a = 4;
+            int b = 0;
+            int expected = 2;
+            Calculator calculator = new Calculator();
+            // When
+            int actual = 0;
+            // Then
+            Assert.Throws<ArgumentException>(() =>
+            actual = calculator.Devide(a, b)
+           );
         }
     }
 }
--- Aufgaben/01-unittest/Calculator/Calculator.cs
+++ Aufgaben/01-unittest/Calculator/Calculator.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 
+
 namespace Calculator
 {
     public class Calculator
     {
         public int Add(int summandA, int summandB)
         {
-            return summandA + summandB;
+            return summandA + summandB;//+1
         }
-
         public int Subtract(int minuend, int subtrahend)
         {
             return minuend - subtrahend;
+        }
+        public int Multiply(int multiplientA, int multiplientB)
+        {
+            return multiplientA * multiplientB;
+        }
+        public int Devide(int DividentA, int DividentB)
+        {
+            if (DividentA == 0 || DividentB == 0)
+            {
+                throw new ArgumentException();
+            }
+            return DividentA / DividentB;
         }
     }
 }
